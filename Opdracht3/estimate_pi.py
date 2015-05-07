555660308b057dc29cd822a1eafad1e2c82a268d
Arian van Putten <aeroboy94@gmail.com>
2015-05-07 15:58:31 +0200

    Nu is l gewoon prima

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -2,39 +2,41 @@ import random
 import sys
 
 from math import sin, cos
-s = int(sys.argv[3])
-random.seed(s)
-l = float(sys.argv[2])
-n = int(sys.argv[1])
 
-assert(l <= 1)
+try:
+    s = int(sys.argv[3])
+    random.seed(s)
+    l = float(sys.argv[2])
+    n = int(sys.argv[1])
 
+    assert(l <= 1)
 
+    def drop_needle(l):
+        x0 = random.random()
+        phi = random.vonmisesvariate(0,0)
 
+        x = x0 + l*cos(phi)
 
+        start = min(x0,x)
+        end = max(x0,x)
 
-def drop_needle(l):
-    x0 = math.random()
-    phi = random.vonmisesvariate(0,0)
+        if start <= 0:
+            return end >= 0
 
-    x = x0 + l*cos(phi)
-
-    
-    start = min(x0,x)
-    end = max(x0,x)
-
-    if start <= 0:
-        return end >= 0
-
-    if start <= 1:
-        return end >= 1
+        if start <= 1:
+            return end >= 1
 
 
-hits = 0
-for i in range(0,n):
-    if drop_needle(l):
-        hits += 1
+    hits = 0
+    for i in range(0,n):
+        if drop_needle(l):
+            hits += 1
 
 
-print( 2* (float(n) / float(hits)))
-print("{}/ {}".format(hits,n))
+    pi = 2 * l *(float(n) / float(hits))
+
+
+    print("{} hits in {} tries".format(hits,n))
+    print("Pi = {}".format(pi))
+except:
+    print("Use: estimate_py N L S")
