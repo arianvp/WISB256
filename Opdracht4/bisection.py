3aea5aefaa33be84594e3025ffb3cb6a4ac4eea1
Arian van Putten <aeroboy94@gmail.com>
2015-05-21 13:51:18 +0200

    find all roots

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,12 +1,26 @@
 def findRoot(f, a, b, epsilon):
     m = ( a + b ) / 2
 
-    if f(m)     == 0 or abs(b - a) <= epsilon:
+    if f(m) == 0 or abs(b - a) <= epsilon:
         return m
     if f(a)*f(m) <= 0:
         return findRoot(f,a,m,epsilon)
     else:
         return findRoot(f,m,b,epsilon)
+
+# Take the nth derivative
+def findAllRoots(f, a, b, epsilon):
+    roots = []
+    r = findRoot(f,a,b,epsilon)
+    roots.append(r)
+    while abs(r - b) > epsilon:
+        r = findRoot(f,r+0.1,b,epsilon)
+        roots.append(r)
+
+    roots.pop() # remove debris
+    return roots
+        
+
 
     
 
